ed226210e20fdf7bc99fc160224f30e40f4a73c4
[your <hartlanddevelop@gmail.com>
2021-12-11 01:51:04 -0800

    left off

--- components/Stories.js
+++ components/Stories.js
@@ -1,19 +1,26 @@
 import faker, { fake } from "faker";
-import {useEffect }  from 'react'
+import { useEffect, useState } from "react";
+import Story from "./Story";
 
 function Stories() {
+  const [suggestions, setSuggestions] = useState([]);
   useEffect(() => {
-      const suggestions = [...Array(20)].map((_, i) => ({
-          ...faker.helpers.contextualCard(),
-          id: i,
-      }));
-
-      console.log(suggestions)
-  })
+    const suggestions = [...Array(20)].map((_, i) => ({
+      ...faker.helpers.contextualCard(),
+      id: i,
+    }));
+    setSuggestions(suggestions);
+  }, []);
 
   return (
     <div>
-      {/* Story  */}
+      {suggestions.map((profile) => (
+        <Story
+          key={profile.id}
+          image={profile.avatar}
+          username={profile.username}
+        />
+      ))}
       {/* Story  */}
       {/* Story  */}
       {/* Story  */}
---
+++ components/Story.js
@@ -0,0 +1,10 @@
+function Story({ image, username }) {
+  return (
+    <div>
+      <img src="" alt="profile image" />
+      <p>{username}</p>
+    </div>
+  );
+}
+
+export default Story;
